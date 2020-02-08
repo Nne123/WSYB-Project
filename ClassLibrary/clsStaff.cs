c1c607976bb350e48466060b200fad6083e4bdc8
Nneka Udeh <P17210378@my365.dmu.ac.uk>
2020-02-08 17:53:30 +0000

    Without Azure Connection

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,11 +1,129 @@
-﻿namespace ClassLibrary
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
-        public int StaffNo { get; set; }
-        public string StaffFirstName { get; set; }
-        public string StaffLastName { get; set; }
-        public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
+        // private data member for the StaffNo
+        private Int32 mStaffNo;
+        private string mFirstName;
+        private string mLastName;
+        private string mAddressLine;
+        private string mPostCode;
+        private Int32 mCountyNo;
+        private string mPhoneNo;
+        private Boolean mActive;
+
+        public int StaffNo
+        {
+            get
+            {
+                return mStaffNo;
+            }
+            set
+            {
+                mStaffNo = value;
+            }
+        }
+
+        public string StaffFirstName
+        {
+            get
+            {
+                return mFirstName;
+            }
+            set
+            {
+                mFirstName = value;
+            }
+        }
+        public string StaffLastName
+        {
+            get
+            {
+                return mLastName;
+            }
+            set
+            {
+                mLastName = value;
+            }
+        }
+        public string AddressLine
+        {
+            get
+            {
+                return mAddressLine;
+            }
+            set
+            {
+                mAddressLine = value;
+            }
+        }
+        public int CountyNo
+        {
+            get
+            {
+                return mCountyNo;
+            }
+            set
+            {
+                mCountyNo = value;
+            }
+        }
+        public string PostCode
+        {
+            get
+            {
+                return mPostCode;
+            }
+            set
+            {
+                mPostCode = value;
+            }
+        }
+
+       public string PhoneNo
+        {
+            get
+            {
+                return mPhoneNo;
+            }
+            set
+            {
+                mPhoneNo = value;
+            }
+        }
+
+        public bool Active
+        {
+            get
+            {
+                return mActive;
+            }
+            set
+            {
+                mActive = value;
+            }
+        }
+
+ 
+
+        public bool Find(int StaffNo)
+        {
+            // set the private data member to the test data value
+            mStaffNo = 1;
+            mFirstName = "Brad";
+            mLastName = "Pitt";
+            mAddressLine = "6 Grange Lane";
+            mPostCode = "4HG 57G";
+            mCountyNo = 1;
+            mPhoneNo = "0745375957";
+            mActive = true;
+            // always return true
+            return true;
+        }
     }
 }
---
+++ TestFramework/tstCountyCollection.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TestFramework
+{
+    [TestClass]
+    public class tstCountyCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsCountyCollection AllCounties = new clsCountyCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllCounties);
+        }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCountyCollection AllCounties = new clsCountyCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 72;
+            //assign the data to the property
+            AllCounties.Count = SomeCount;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCounties.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void AllCountiesOK()
+        {
+            //create an instance of the class we want to create
+            clsCountyCollection Counties = new clsCountyCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsCounty> TestList = new List<clsCounty>();
+            //add an item to the list
+            //create the item of test data
+            clsCounty TestItem = new clsCounty();
+            //set its properties
+            TestItem.CountyNo = 1;
+            TestItem.County = "Leicestershire";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            Counties.AllCounties = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(Counties.AllCounties, TestList);
+        }
+
+        [TestMethod]
+        public void CountMatchesList()
+        {
+            //create an instance of the class we want to create
+            clsCountyCollection Counties = new clsCountyCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsCounty> TestList = new List<clsCounty>();
+            //add an item to the list
+            //create the item of test data
+            clsCounty TestItem = new clsCounty();
+            //set its properties
+            TestItem.CountyNo = 1;
+            TestItem.County = "Leicestershire";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            Counties.AllCounties = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(Counties.Count, TestList.Count);
+        }
+
+        //[TestMethod]
+        //public void TwoCountiesPresent()
+        //{
+        //    //create an instance of the class we want to create
+        //    clsCountyCollection Counties = new clsCountyCollection();
+        //    //test to see that the two values are the same
+        //    Assert.AreEqual(Counties.Count, 2);
+        //}
+    }
+}
